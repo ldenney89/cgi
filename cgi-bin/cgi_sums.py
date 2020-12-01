e34938fdd72d3d2c803d2565eeccfa9c40ba85d6
Laura Denney <laura.a.denney@gmail.com>
2020-11-30 19:47:06 -0800

    updated cipbin/cig_sums.py and added field_storage.py

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -5,5 +5,15 @@ import cgitb
 cgitb.enable()
 
 print("Content-type: text/plain")
-print()
-print("Your job is to make this work")
+print("")
+
+form = cgi.FieldStorage()
+listval = form.getlist('operand')
+try:
+    intlist = [int(x) for x in listval]
+    body = sum(intlist)
+except (ValueError, TypeError):
+    body = "Unable to calculate a sum, please provide integer operands."
+#print("Your job is to make this work")
+print("You provided the numbers {}.".format(str(listval)))
+print("The sum of the numbers is: {}".format(body))
